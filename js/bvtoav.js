f579fd56173b73fb63df861fe1849e58adccd6b3
mogmoug <88359334+mogmoug@users.noreply.github.com>
2021-08-06 11:27:21 +0800

    Create bvtoav.js

---
+++ js/bvtoav.js
@@ -0,0 +1,28 @@
+var table='fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF';
+var tr= new Array();
+
+for (i in range(58)){
+	tr[table[i]]=i;
+  }
+var s=[11,10,3,8,4,6];
+var xor=177451812;
+var add=8728348608;
+
+funtion dec(x){
+	r=0;
+	for i in range(6){
+		r+=tr[x[s[i]]]*58**i;
+    }
+	return (r-add)^xor;
+}
+function enc(x){
+	x=(x^xor)+add;
+	r=list('BV1  4 1 7  ');
+	for i in range(6){
+		r[s[i]]=table[x//58**i%58];
+    }
+	return ''.join(r);
+}
+//作者：mcfx
+//链接：https://www.zhihu.com/question/381784377/answer/1099438784
+//来源：知乎
